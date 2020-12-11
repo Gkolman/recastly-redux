bd95e4426420edd208990613d33a925b1b249bef
Gkolman <Gkolman17@gmail.com>
2020-12-10 19:20:07 -0800

    Complete Bare Minimum Requirements

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './../reducers/main.js';
-import exampleVideoData from '../data/exampleVideoData.js';
 
+// var store = createStore(rootReducer, applyMiddleware(thunk));
 var store = createStore(rootReducer, applyMiddleware(thunk));
 
 
 
-
+//
 export default store;
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
